f3428ed6cc2055d9570f362b9e9726a31881ab2a
kingsley ijomah <kingsley.ijomah@gmail.com>
2017-04-17 17:30:29 +0100

    packaging

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from flask impport Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
